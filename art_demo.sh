e1d26bac6c6ce9e58abdc032c4b3e273609c337a
Jason Smith <jsmith@401m.net>
2018-09-27 14:45:07 +0000

    Added art messages files

---
+++ art_demo.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+WHITE="\033[0;37m"
+BLUE="\033[0;34m"
+YELLOW="\033[0;33m"
+CYAN="\033[0;36m"
+PURPLE="\033[0;35m"
+RED='\033[0;31m'
+GREEN="\033[0;32m"
+NC='\033[0m'
+Off='\E[0m'
+Bold='\E[1m'
+Dim='\E[2m'
+Underline='\E[4m'
+Blink='\E[5m'
+Reverse='\E[7m'
+Strike='\E[9m'
+FgBlack='\E[39m'
+FgRed='\E[31m'
+FgGreen='\E[32m'
+FgYellow='\E[33m'
+FgBlue='\E[34m'
+FgMagenta='\E[35m'
+FgCyan='\E[36m'
+FgWhite='\E[37m'
+BgBlack='\E[40m'
+BgRed='\E[41m'
+BgGreen='\E[42m'
+BgYellow='\E[43m'
+BgBlue='\E[44m'4
+BgMagenta='\E[45m'
+BgCyan='\E[46m'
+BgWhite='\E[47m'
+FgLtBlack='\E[90m'
+FgLtRed='\E[91m'
+FgLtGreen='\E[92m'
+FgLtYellow='\E[93m'
+FgLtBlue='\E[94m'
+FgLtMagenta='\E[95m'
+FgLtCyan='\E[96m'
+FgLtWhite='\E[97m'
+BgLtBlack='\E[100m'
+BgLtRed='\E[101m'
+BgLtGreen='\E[102m'
+BgLtYellow='\E[103m'
+BgLtBlue='\E[104m'
+BgLtMagenta='\E[105m'
+BgLtCyan='\E[106m'
+BgLtWhite='\E[107m'
+
+
+echo -e "        **             ***** *     **          * ***         ***** *     **   "
+echo -e "     *****          ******  **    **** *     *  ****      ******  **    **** *"
+echo -e "    *  ***         **   *  * **    ****     *  *  ***    **   *  * **    **** "
+echo -e "       ***        *    *  *  **    * *     *  **   ***  *    *  *  **    * *  "
+echo -e "      *  **           *  *    **   *      *  ***    ***     *  *    **   *    "
+echo -e "      *  **          ** **    **   *     **   **     **    ** **    **   *    "
+echo -e "     *    **         ** **     **  *     **   **     **    ** **     **  *    "
+echo -e "     *    **         ** **     **  *     **   **     **    ** **     **  *    "
+echo -e "    *      **        ** **      ** *     **   **     **    ** **      ** *    "
+echo -e "    *********        ** **      ** *     **   **     **    ** **      ** *    "
+echo -e "   *        **       *  **       ***      **  **     **    *  **       ***    "
+echo -e "   *        **          *        ***       ** *      *        *        ***    "
+echo -e "  *****      **     ****          **        ***     *     ****          **    "
+echo -e " *   ****    ** *  *  *****                  *******     *  *****             "
+echo -e "*     **      **  *     **                     ***      *     **              "
+echo -e "*                 *                                     *                     "
+echo -e " **                **                                    **                   "
